f7d9e8a225f62b8cb550577798a779b219e45cf6
Ildar Mosnabeev <web.udm@gmail.com>
2021-05-02 14:03:29 +0400

    Добавил функцию для получения максимального количество страниц в топик-листе

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
 <?php
+
+declare(strict_types=1);
+
+use Symfony\Component\DomCrawler\Crawler;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+function getCrawlerByUrl(string $url): Crawler {
+    return new Crawler(file_get_contents($url));
+}
+
+function getMaxPage(Crawler $crawlerOfTopicsListPage): int {
+    return (int) $crawlerOfTopicsListPage->filter('.PageNavNext + a')->text();
+}
+
+print_r(
+    getMaxPage(
+        getCrawlerByUrl('https://php.ru/forum/forums/php-dlja-novichkov.13/')));
