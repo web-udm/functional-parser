930a0e65bf54d21b95939b9303a71de6b6dd1167
Ildar Mosnabeev <web.udm@gmail.com>
2021-05-02 14:14:28 +0400

    Добавил функцию для генерации урлов страниц из темы

--- index.php
+++ index.php
@@ -6,14 +6,23 @@ use Symfony\Component\DomCrawler\Crawler;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-function getCrawlerByUrl(string $url): Crawler {
-    return new Crawler(file_get_contents($url));
+function getCrawlerByUrl(string $topicsListUrl): Crawler {
+    return new Crawler(file_get_contents($topicsListUrl));
 }
 
 function getMaxPage(Crawler $crawlerOfTopicsListPage): int {
     return (int) $crawlerOfTopicsListPage->filter('.PageNavNext + a')->text();
 }
 
-print_r(
-    getMaxPage(
-        getCrawlerByUrl('https://php.ru/forum/forums/php-dlja-novichkov.13/')));
+function getForumPages(string $topicsListUrl): array {
+    $maxPageNumber = getMaxPage(getCrawlerByUrl($topicsListUrl));
+    $pagesArr = [];
+
+    foreach (range(1, $maxPageNumber) as $pageNumber) {
+        $pagesArr[] = "$topicsListUrl/page-$maxPageNumber";
+    }
+
+    return $pagesArr;
+}
+
+print_r(getForumPages('https://php.ru/forum/forums/php-dlja-novichkov.13'));
