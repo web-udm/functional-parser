3f2112078c32b1af86aea1f8661f8df632be8f57
Ildar Mosnabeev <web.udm@gmail.com>
2021-05-02 14:56:21 +0400

    Добавил кэширование при получении html-страниц

--- index.php
+++ index.php
@@ -6,23 +6,41 @@ use Symfony\Component\DomCrawler\Crawler;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-function getCrawlerByUrl(string $topicsListUrl): Crawler {
-    return new Crawler(file_get_contents($topicsListUrl));
+function getHtml(string $url): string {
+    $filePath = __DIR__ . '/cache/' . md5($url);
+
+    if (file_exists($filePath)) {
+        return unserialize(file_get_contents($filePath));
+    }
+
+    $html = file_get_contents($url);
+    file_put_contents($filePath, serialize($html));
+
+    return $html;
+}
+
+function getCrawler(string $html): Crawler {
+    return new Crawler($html);
 }
 
 function getMaxPage(Crawler $crawlerOfTopicsListPage): int {
     return (int) $crawlerOfTopicsListPage->filter('.PageNavNext + a')->text();
 }
 
-function getForumPages(string $topicsListUrl): array {
-    $maxPageNumber = getMaxPage(getCrawlerByUrl($topicsListUrl));
-    $pagesArr = [];
-
-    foreach (range(1, $maxPageNumber) as $pageNumber) {
-        $pagesArr[] = "$topicsListUrl/page-$maxPageNumber";
-    }
-
-    return $pagesArr;
+function getForumPagesUrls(string $topicsListUrl): array {
+    return array_map(function ($pageNumber) use ($topicsListUrl) {
+        return "$topicsListUrl/page-$pageNumber";
+    }, range(1, getMaxPage(getCrawler($topicsListUrl))));
 }
 
-print_r(getForumPages('https://php.ru/forum/forums/php-dlja-novichkov.13'));
+function getTopicsUrlsFromForumPage(string $topicsListUrl): array {
+    return getCrawler($topicsListUrl)->filter('.PreviewTooltip')
+        ->each(function (Crawler $topicCrawler) {
+            return 'https://php.ru/forum/' . $topicCrawler->attr('href');
+        });
+}
+
+//print_r(getTopicsUrlsFromForumPage('https://php.ru/forum/forums/php-dlja-novichkov.13/'));
+
+$crawler = getCrawler(getHtml('https://php.ru/forum/forums/php-dlja-novichkov.13/'));
+print_r($crawler->html());
