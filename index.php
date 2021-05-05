9d66ad2e6335e3a4495cec1d57de0c4dd2110df6
moildar <i.mosnabeev@taxi-master.com>
2021-05-05 18:26:13 +0400

    Добавил функцию для сбора всех тредов из темы

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ use Symfony\Component\DomCrawler\Crawler;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+$url = 'https://php.ru/forum/forums/ide.46';
+
 function getHtml(string $url): string {
     $filePath = __DIR__ . '/cache/' . md5($url);
 
@@ -27,17 +29,23 @@ function getMaxPage(string $url): int {
     return (int) getCrawler($url)->filter('.PageNav nav > a:nth-last-child(2)')->text();
 }
 
-function getForumPagesUrls(string $topicsListUrl): array {
+function getForumPages(string $topicsListUrl): array {
     return array_map(function ($pageNumber) use ($topicsListUrl) {
         return "$topicsListUrl/page-$pageNumber";
     }, range(1, getMaxPage(($topicsListUrl))));
 }
 
-function getTopicsUrlsFromForumPage(string $topicsListUrl): array {
+function getThreadsFromOnePage(string $topicsListUrl): array {
     return getCrawler($topicsListUrl)->filter('.PreviewTooltip')
         ->each(function (Crawler $topicCrawler) {
             return 'https://php.ru/forum/' . $topicCrawler->attr('href');
         });
 }
 
-print_r(getMaxPage('https://php.ru/forum/forums/ide.46/'));
+function getAllThreads(string $forumUrl): array {
+    return array_reduce(getForumPages($forumUrl), function($resArray, $item) {
+        return array_merge($resArray, getThreadsFromOnePage($item));
+    }, []);
+}
+
+print_r(getAllThreads($url));
